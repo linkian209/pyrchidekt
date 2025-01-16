e9394765d10b432020b87c627f8f8f72d0013c33
HaHeho <hannes.helmholz@chalmers.se>
2025-01-16 02:55:45 +0100

    Update deck `Format` and card `LegalFormat` names to be consistent

--- pyrchidekt/formats.py
+++ pyrchidekt/formats.py
@@ -11,21 +11,21 @@ class Format(IntEnum):
     """
     STANDARD = 1
     MODERN = 2
-    COMMANDER_EDH = 3
+    COMMANDER = 3
     LEGACY = 4
     VINTAGE = 5
     PAUPER = 6
     CUSTOM = 7
     FRONTIER = 8
     FUTURE_STANDARD = 9
-    PENNY_DREADFUL = 10
+    PENNY = 10
     ONE_V_ONE_COMMANDER = 11
-    DUAL_COMMANDER = 12
+    DUEL_COMMANDER = 12
     BRAWL = 13
     OATHBREAKER = 14
     PIONEER = 15
     HISTORIC = 16
-    PAUPER_EDH = 17
+    PAUPER_COMMANDER = 17
     ALCHEMY = 18
     EXPLORER = 19
     HISTORIC_BRAWL = 20
@@ -42,11 +42,11 @@ class LegalFormat(StrEnum):
     """
     ALCHEMY = "alchemy"
     LEGACY = "legacy"
-    OLD_SCHOOL = "oldschool"
+    FRONTIER = "oldschool"
     MODERN = "modern"
     VINTAGE = "vintage"
     OATHBREAKER = "oathbreaker"
-    ONE_V_ONE = "1v1"
+    ONE_V_ONE_COMMANDER = "1v1"
     HISTORIC_BRAWL = "historicbrawl"
     PREMODERN = "premodern"
     HISTORIC = "historic"
@@ -57,10 +57,10 @@ class LegalFormat(StrEnum):
     BRAWL = "brawl"
     PENNY = "penny"
     PIONEER = "pioneer"
-    DUEL = "duel"
+    DUEL_COMMANDER = "duel"
     PAUPER = "pauper"
     STANDARD = "standard"
-    FUTURE = "future"
+    FUTURE_STANDARD = "future"
     PREDH = "predh"
     TIMELESS = "timeless"
     CANADIAN_HIGHLANDER = "canlander"
